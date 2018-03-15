1e83e3983dfe96a2ffeb0130069fa65fee428233
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2018-03-15 14:15:36 +0900

    ssh_config

--- install.sh
+++ install.sh
@@ -99,7 +99,11 @@ start_docker() {
 build_ssh() {
     if [ ! -f ~/.ssh/id_rsa ]; then
         ssh-keygen -q -f ~/.ssh/id_rsa -N ""
+
         cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+        curl -s -o ~/.ssh/config https://gitlab.com/nalbam/openshift/raw/master/config
+
         ssh -o StrictHostKeyChecking=no ${USERNAME}@${IP} "pwd" < /dev/null
     fi
 }
