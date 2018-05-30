dfaaa2d3d4a32f6850601c50c0481f7a018c241b
nalbam <me@nalbam.com>
2018-05-30 21:45:46 +0900

    add_nexus3_redhat_repos

--- template/nexus-functions.sh
+++ template/nexus-functions.sh
@@ -23,8 +23,8 @@ function add_nexus2_repo() {
 {
    "data": {
       "repoType": "proxy",
-      "id": "$_REPO_ID",
-      "name": "$_REPO_ID",
+      "id": "${_REPO_ID}",
+      "name": "${_REPO_ID}",
       "browseable": true,
       "indexable": true,
       "notFoundCacheTTL": 1440,
@@ -40,7 +40,7 @@ function add_nexus2_repo() {
       "exposed": true,
       "checksumPolicy": "WARN",
       "remoteStorage": {
-         "remoteStorageUrl": "$_REPO_URL",
+         "remoteStorageUrl": "${_REPO_URL}",
          "authentication": null,
          "connectionSettings": null
       }
@@ -63,7 +63,7 @@ function add_nexus2_group_repo() {
   local _NEXUS_URL=$5
 
   GROUP_JSON=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" -f -X GET -u "$_NEXUS_USER:$_NEXUS_PWD" "$_NEXUS_URL/service/local/repo_groups/$_GROUP_ID")
-  GROUP_JSON_WITH_REPO=$(echo $GROUP_JSON | sed "s/\"repositories\":\[/\"repositories\":[{\"id\": \"$_REPO_ID\"},/g")
+  GROUP_JSON_WITH_REPO=$(echo ${GROUP_JSON} | sed "s/\"repositories\":\[/\"repositories\":[{\"id\": \"$_REPO_ID\"},/g")
   curl -v -f -X PUT -H "Accept: application/json" -H "Content-Type: application/json" -d "$GROUP_JSON_WITH_REPO" -u "$_NEXUS_USER:$_NEXUS_PWD" "$_NEXUS_URL/service/local/repo_groups/$_GROUP_ID"
 }
 
@@ -76,15 +76,15 @@ function add_nexus2_redhat_repos() {
   local _NEXUS_PWD=$2
   local _NEXUS_URL=$3
 
-  add_nexus2_repo redhat-ga https://maven.repository.redhat.com/ga/ $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
-  add_nexus2_repo redhat-ea https://maven.repository.redhat.com/earlyaccess/all/ $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
-  add_nexus2_repo redhat-techpreview https://maven.repository.redhat.com/techpreview/all $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
-  add_nexus2_repo jboss-ce https://repository.jboss.org/nexus/content/groups/public/ $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
+  add_nexus2_repo redhat-ga https://maven.repository.redhat.com/ga/ ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
+  add_nexus2_repo redhat-ea https://maven.repository.redhat.com/earlyaccess/all/ ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
+  add_nexus2_repo redhat-tp https://maven.repository.redhat.com/techpreview/all/ ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
+  add_nexus2_repo jboss-ce https://repository.jboss.org/nexus/content/groups/public/ ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
 
-  add_nexus2_group_repo jboss-ce public $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
-  add_nexus2_group_repo redhat-ga public $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
-  add_nexus2_group_repo redhat-ea public $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
-  add_nexus2_group_repo redhat-techpreview public $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
+  add_nexus2_group_repo redhat-ga public ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
+  add_nexus2_group_repo redhat-ea public ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
+  add_nexus2_group_repo redhat-tp public ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
+  add_nexus2_group_repo jboss-ce public ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
 }
 
 
@@ -100,9 +100,9 @@ function add_nexus3_repo() {
 
   read -r -d '' _REPO_JSON << EOM
 {
-  "name": "$_REPO_ID",
+  "name": "${_REPO_ID}",
   "type": "groovy",
-  "content": "repository.createMavenProxy('$_REPO_ID','$_REPO_URL')"
+  "content": "repository.createMavenProxy('${_REPO_ID}','${_REPO_URL}')"
 }
 EOM
 
@@ -127,9 +127,9 @@ function add_nexus3_group_repo() {
 
   read -r -d '' _REPO_JSON << EOM
 {
-  "name": "$_GROUP_ID",
+  "name": "${_GROUP_ID}",
   "type": "groovy",
-  "content": "repository.createMavenGroup('$_GROUP_ID', '$_REPO_IDS'.split(',').toList())"
+  "content": "repository.createMavenGroup('${_GROUP_ID}', '${_REPO_IDS}'.split(',').toList())"
 }
 EOM
 
@@ -150,10 +150,10 @@ function add_nexus3_redhat_repos() {
   local _NEXUS_PWD=$2
   local _NEXUS_URL=$3
 
-  add_nexus3_repo redhat-ga https://maven.repository.redhat.com/ga/ $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
-  add_nexus3_repo redhat-ea https://maven.repository.redhat.com/earlyaccess/all/ $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
-  add_nexus3_repo redhat-techpreview https://maven.repository.redhat.com/techpreview/all $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
-  add_nexus3_repo jboss-ce https://repository.jboss.org/nexus/content/groups/public/ $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
+  add_nexus3_repo redhat-ga https://maven.repository.redhat.com/ga/ ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
+  add_nexus3_repo redhat-ea https://maven.repository.redhat.com/earlyaccess/all/ ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
+  add_nexus3_repo redhat-tp https://maven.repository.redhat.com/techpreview/all/ ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
+  add_nexus3_repo jboss-ce https://repository.jboss.org/nexus/content/groups/public/ ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
 
-  add_nexus3_group_repo maven-central,maven-releases,maven-snapshots,jboss-ce,redhat-ga,redhat-ea,redhat-techpreview maven-all-public $_NEXUS_USER $_NEXUS_PWD $_NEXUS_URL
+  add_nexus3_group_repo maven-central,maven-releases,maven-snapshots,jboss-ce,redhat-ga,redhat-ea,redhat-techpreview maven-all-public ${_NEXUS_USER} ${_NEXUS_PWD} ${_NEXUS_URL}
 }
